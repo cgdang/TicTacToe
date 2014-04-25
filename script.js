6a74f2cfce5e47f2e5139ce7ca7d4f7bd28f55db
Clarence Dang <cdang@Clarences-MacBook-Pro.local>
2014-04-24 18:49:41 -0700

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,93 @@
 function TictactoeController($scope) {
-	$scope.board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-	$scope.clicker = function(cellIndex) {
-		$scope.board[cellIndex]="X";	
+	$scope.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+	var clickedSpaces = 0;
+
+	var win = false;
+
+	var checkWin = function() {
+	if ($scope.board[0] == $scope.board[1] && $scope.board[2] == $scope.board[1] && $scope.board[1] != " ") {
+		win = true;
+		// alert("MJ WINS");
 	}
+	if ($scope.board[3] == $scope.board[4] && $scope.board[5] == $scope.board[4] && $scope.board[4] != " ") {
+		win = true;
+		// alert("MJ WINS");
+	}
+	if ($scope.board[6] == $scope.board[7] && $scope.board[8] == $scope.board[7] && $scope.board[7] != " ") {
+		win = true;
+		// alert("MJ WINS");
+	}
+	if ($scope.board[0] == $scope.board[3] && $scope.board[6] == $scope.board[3] && $scope.board[3] != " ") {
+		win = true;
+		// alert("MJ WINS");
+	}
+	if ($scope.board[1] == $scope.board[4] && $scope.board[7] == $scope.board[4] && $scope.board[4] != " ") {
+		win = true;
+		// alert("MJ WINS");
+	}
+	if ($scope.board[2] == $scope.board[5] && $scope.board[8] == $scope.board[5] && $scope.board[5] != " ") {
+		win = true;
+		// alert("MJ WINS");
+	}
+	if ($scope.board[0] == $scope.board[4] && $scope.board[8] == $scope.board[4] && $scope.board[4] != " ") {
+		win = true;
+		// alert("MJ WINS");
+	}
+	if ($scope.board[2] == $scope.board[4] && $scope.board[6] == $scope.board[4] && $scope.board[4] != " ") {
+		win = true;
+		// alert("MJ WINS");
+	} else if(clickedSpaces == 9  && win === false) {
+		// alert("DRAW");
+	}
+	};
+
+	$scope.clicker = function(cellIndex) {
+		if ($scope.board[cellIndex] == " ") {
+			clickedSpaces++;
+			if (clickedSpaces % 2 === 0) {
+				$scope.board[cellIndex] = "o";
+				$scope.board.push[cellIndex];
+				console.log($scope.board);
+				checkWin();
+
+			} else {
+				$scope.board[cellIndex] = "x";
+				$scope.board.push[cellIndex];
+				console.log($scope.board);
+				checkWin();
+			}	
+		} else {
+			// alert("MJ, PICK ANOTHER ONE");
+		}	
+	};
+
+	$scope.reset = function() {
+		$scope.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+		clickedSpaces = 0;
+		win = false;
+	};
+
+	$scope.mjwins = function() {
+		if (win === true)
+			return true;
+		else
+			return false;
+	};
+
+	$scope.draw = function() {
+		if (clickedSpaces == 9  && win === false)
+			return true;
+		else
+			return false;
+	};
+
+	// $scope.invalidClick = function() {
+	// 	if ($scope.clicker(cellIndex) === false)
+	// 		return true;
+	// 	else
+	// 		return false;
+	// };
+
 }
+
